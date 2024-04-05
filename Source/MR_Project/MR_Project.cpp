45a57bed0cbc8211d2f5ac4ac51ac2ccb86981eb
Oyungerel Jargalsaikhan <lizzbetgrant1009@gmail.com>
2024-04-05 14:30:35 +0800

    Initial commit

---
+++ Source/MR_Project/MR_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MR_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MR_Project, "MR_Project" );
---
+++ Source/MR_Project/MR_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MR_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MR_PROJECT_API AMR_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
